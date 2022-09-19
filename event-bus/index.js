8add5a8016cf7fe3a327f0d0c132d21e3c9c8537
afzalRapidInnovation <afzalmansuri@rapidinnovation.dev>
2022-09-19 17:38:56 +0530

    comment moderationa and saving events in bus

--- event-bus/index.js
+++ event-bus/index.js
@@ -6,17 +6,39 @@ const app = express();
 const port = 4005;
 
 app.use(bodyParser.json());
+const events = []
 
 app.post("/events", (req, res) => {
   const event = req.body;
 
-  axios.post("http://localhost:4000/events", event);
-  axios.post("http://localhost:4001/events", event);
-  axios.post("http://localhost:4002/events",event);
-  axios.post("http://localhost:4003/events", event);
+  events.push(event)
+  try{
+    axios.post("http://localhost:4000/events", event).catch((e)=> {
+      console.log(e)
+    });
+  axios.post("http://localhost:4001/events", event).catch((e)=> {
+    console.log(e)
+  });
+  axios.post("http://localhost:4002/events",event).catch((e)=> {
+    console.log(e)
+  });
+  axios.post("http://localhost:4003/events", event).catch((e)=> {
+    console.log(e)
+  });
 
+  }catch(e){
+    console.log(e)
+  }
+
+
+  
   res.send({ status: "OK" });
 });
+
+app.get("/events",(req,res) => {
+  res.send(events)
+})
+
 
 app.listen(port, () => {
   console.log(`Event-bus server is up on port :${port}`);
--- query/index.js
+++ query/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser =  require('body-parser')
 const cors =  require('cors')
+const axios = require('axios')
 
 const app = express() 
 const port = 4002
@@ -25,30 +26,54 @@ const posts = {}
 //     },
 //   };
 
+const handleEvent = (type,data) => {
+    if (type === "PostCreated") {
+      const { id, title } = data;
+      posts[id] = { id, title, comments: [] };
+    }
+    if (type === "CommentCreated") {
+      const { id, content, postId, status } = data;
+      let post = posts[postId];
+      post.comments.push({ id, content, status });
+    }
+
+    if (type === "CommentUpdated") {
+      const { id, content, postId, status } = data;
+      const post = posts[postId];
+      const comment = post.comments.find((comment) => {
+        return comment.id === id;
+      });
+
+      comment.status = status;
+      comment.content = content;
+    }
+}
+
 
 app.get('/posts',(req,res)=>{
     res.send(posts)
 })
 
 app.post('/events',(req,res) => {
-    const { type, data } = req.body
-
-    if( type === 'PostCreated'){
-        const { id, title } = data
-        posts[id] = { id, title, comments:[]}
-    }
-    if (type === "CommentCreated") {
-        const { id, content, postId ,status } = data
-        let post = posts[postId]
-        post.comments.push({id,content,status})
-    }
-
+    const { type, data } = req.body;
     // console.log(posts)
+    handleEvent(type,data);
 
-    res.send({})
+    res.send({});
 })
 
 
-app.listen(port,() => {
+app.listen(port, async() => {
     console.log(`Query server is up on port :${port}`)
+    try {
+      const res = await axios.get("http://localhost:4005/events");
+
+      for (let event of res.data) {
+        console.log("Processing event:", event.type);
+
+        handleEvent(event.type, event.data);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
 })
